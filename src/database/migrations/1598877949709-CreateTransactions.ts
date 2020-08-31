a2f7b04ace2b9d5df341580a205712f8511c108e
LeonardoCHb <leonardohably@hotmail.com>
2020-08-31 10:26:13 -0300

    migrations OK! FINALLY

---
+++ src/database/migrations/1598877949709-CreateTransactions.ts
@@ -0,0 +1,51 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTransactions1598877949709 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'transactions',
+                columns: [
+                    {
+                        name:'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()'
+                    },
+                    {
+                        name: 'titile',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'type',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'value',
+                        type: 'decimal',
+                        precision: 10,
+                        scale: 2,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'update_at',
+                        type: 'timestamp',
+                        default:'now()',
+                    }
+
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('transactions')
+    }
+
+}
